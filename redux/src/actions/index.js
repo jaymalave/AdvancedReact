a988744dff96ede55bfbe1bfa14f021f67d86122
jaymalave <jaym.github@gmail.com>
2021-07-05 17:56:23 +0530

    fixed

---
+++ redux/src/actions/index.js
@@ -0,0 +1,16 @@
+export const increment = (number) => {
+    return {
+    type: 'INCREMENT',
+    payload: number
+    }
+}
+export const decrement = (number) => {
+    return {
+    type: 'DECREMENT',
+    payload: number
+    }
+}
+
+
+
+
