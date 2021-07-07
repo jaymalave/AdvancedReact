097f219e5e859fcb8da41d7956a5c60c7b29daac
jaymalave <jaym.github@gmail.com>
2021-07-07 18:45:14 +0530

    added context-api

--- context-api/src/App.js
+++ context-api/src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
+import { MovieProvider } from './components/MovieContext'
 import MovieList from './components/MovieList'
 
 
 function App() {
   return (
+    <MovieProvider>
     <div className="App">
      <MovieList />
     </div>
+    </MovieProvider>
   );
 }
 
